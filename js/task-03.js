eadfb90c813fe0691866495784a310dca3c89caa
Artchem <chemerys.artem@gmail.com>
2023-07-04 22:18:25 +0300

    update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,20 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryRef = document.querySelector('.gallery');
+
+const markup = images
+  .map(
+    ({ url, alt }) => `<li>
+      <img class = 'image' src="${url}" alt="${alt}">
+      </li>`
+  )
+  .join('');
+
+console.dir(galleryRef);
+console.dir(markup);
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
+
+// galleryRef.innerHTML = '';
