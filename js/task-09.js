acca5475d3df1b09e3146e17a788485c912cfcbf
Artchem <chemerys.artem@gmail.com>
2023-07-16 12:59:41 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -22,10 +22,9 @@ function onBtnChangeColorClick(event) {
   document.body.style.backgroundColor = getRandomHexColor();
 
   textColor.textContent = getRandomHexColor();
-
-  function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215)
-      .toString(16)
-      .padStart(6, 0)}`;
-  }
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
